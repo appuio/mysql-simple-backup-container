e5ef5d1a52f3112fc05d8c9977d22e18a387bf3b
Severin Räz <raez@puzzle.ch>
2017-12-21 15:45:19 +0100

    Do not dump via memory

--- bin/job.sh
+++ bin/job.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 DATE=$(date +%Y-%m-%d-%H-%M)
-dump=$(mysqldump --user=$MYSQL_USER --password=$MYSQL_PASSWORD --host=$MYSQL_SERVICE_HOST $MYSQL_DATABASE)
+
+mysqldump \
+    --user=$MYSQL_USER \
+    --password=$MYSQL_PASSWORD \
+    --host=$MYSQL_SERVICE_HOST \
+    $MYSQL_DATABASE \
+    > /tmp/dump.sql
 
 if [ $? -ne 0 ]; then
     echo "mysqldump not successful: ${DATE}"
     exit 1
 fi
 
-printf '%s' "$dump" | gzip > $BACKUP_DATA_DIR/dump-${DATE}.sql.gz
+gzip -c /tmp/dump.sql > $BACKUP_DATA_DIR/dump-${DATE}.sql.gz
 
 if [ $? -eq 0 ]; then
     echo "backup created: ${DATE}"
@@ -22,3 +28,9 @@ if [ "$old_dumps" ]; then
     echo "Deleting: $old_dumps"
     rm $old_dumps
 fi
+
+function cleanup {
+    rm -f /tmp/dump.sql.gz /tmp/dump.sql
+}
+
+trap cleanup EXIT
